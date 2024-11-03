427b0e646ca1f02c2c80c3e7571baf65907a9dde
KeachT <503.b03@gmail.com>
2024-11-04 01:07:52 +0900

    Chapter 9 Streaming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,7 +15,6 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log("Fetching revenue data...");
-
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
@@ -219,3 +218,6 @@ export async function fetchFilteredCustomers(query: string) {
     throw new Error("Failed to fetch customer table.");
   }
 }
+
+const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
